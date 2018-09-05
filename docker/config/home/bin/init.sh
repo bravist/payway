edd59852b35c67f757bc12f2637076dc742c8f59
会勇禾口王 <chenghuiyong@ruoyubuy.com>
2018-09-05 21:02:51 +0800

    add docker config

---
+++ docker/config/home/bin/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo chown -R root.root /home/worker/nginx/conf/certificates
+sudo chmod 700 /home/worker/nginx/conf/certificates
+sudo chmod 400 /home/worker/nginx/conf/certificates/*
