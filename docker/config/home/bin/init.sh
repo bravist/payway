20b618244a7b4f3cfa67c72661384169ea1801e3
会勇禾口王 <chenghuiyong@ruoyubuy.com>
2018-09-06 09:59:40 +0800

    remove docker compose yml

--- docker/config/home/bin/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo chown -R root.root /home/worker/nginx/conf/certificates
-sudo chmod 700 /home/worker/nginx/conf/certificates
-sudo chmod 400 /home/worker/nginx/conf/certificates/*
