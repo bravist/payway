b9f47d439f680e4fe6618692067a46ae59c1698a
会勇禾口王 <chenghuiyong@ruoyubuy.com>
2018-09-07 14:44:51 +0800

    add new migration event

---
+++ app/Models/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'payment_events';
+}
