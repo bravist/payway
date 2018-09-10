93c3cdffe26d8318d6c3192aed9b5eb82fd9a139
会勇禾口王 <chenghuiyong@ruoyubuy.com>
2018-09-10 12:05:06 +0800

    Create base order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\Api\OrderRequest;
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
 use App\Models\Channel;
+use App\Models\ChannelPayWay;
+use Carbon\Carbon;
 
 class OrderController extends Controller
 {
@@ -39,18 +41,36 @@ class OrderController extends Controller
     {   
         $token = $this->retrieveTokenByRequest($request);
         $channel = Channel::where('client_id', $token->client_id)->first();
+        $payWay = ChannelPayWay::where('payment_channel_id', $channel->id)->where('way', $request->pay_way)->first();
+
         //创建订单基本信息
         $order = Order::create([
             'out_trade_no' => $request->out_trade_no,
             'client_id' => $token->client_id,
             'payment_channel_id' => $channel->id,
             'channel' => $channel->channel,
-            'payway' => $request->out_trade_no,
-            'out_trade_no' => $request->out_trade_no,
-            'out_trade_no' => $request->out_trade_no,
+            'payment_channel_pay_way_id' => $payWay->id,
+            'pay_way' => $request->pay_way,
+            'subject' => $request->subject,
+            'amount' => intval($request->amount),
+            'body' => $request->body,
+            'detail' => $request->detail,
+            'extra' => $request->extra,
+            'buyer' => $request->buyer,
+            'seller' => $request->has('seller') ? $request->seller : $payWay->merchant_id,
+            'pay_at' => Carbon::now(),
+            'expired_at' => Carbon::now()->addHour(2)
         ]);
         //订单号生成并回写
-        $order->update();
+        $orderNo = sprintf(
+            '%s%s', 
+            Carbon::now()->timezone('Asia/Shanghai')->format('YmdHis'), 
+            str_pad($order->id, 4, 0, STR_PAD_LEFT)
+        );
+        $order->update([
+            'trade_no' => $orderNo
+        ]);
+        return $order;
     }
 
 }
