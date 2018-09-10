8144ece5d29c3c85e30a1a3e15313e56e4c443f0
会勇禾口王 <chenghuiyong@ruoyubuy.com>
2018-09-10 09:32:04 +0800

    Order From Request

--- app/Http/Requests/Api/OrderRequest.php
+++ app/Http/Requests/Api/OrderRequest.php
@@ -26,14 +26,16 @@ class OrderRequest extends FormRequest
         switch ($this->method()) {
             case 'POST':
                     return [
+                        'out_trade_no' => 'bail|required|max:64|unique:payment_orders,out_trade_no',
                         'subject' => 'bail|required|max:255',
                         'amount' => 'bail|required|integer|min:1|max:1000000000',
-                        'out_trade_no' => 'bail|required|max:64|min:10',
-                        'channel' => ''
-                        'body' => 'bail|max:128',
+                        'payway' => 'bail|required|string|in:wechat_mini',
+                        'detail' => 'bail|max:255',
+                        'buyer' => 'max:255',
                         'seller_id' => 'max:255',
-                        'buyer_id' => 'max:255',
-                        'goods_detail' => 'bail'
+                        'body' => 'max:255',
+                        'goods_detail' => 'max:255',
+                        'notify_url' => 'bail|required|url',
                     ];
                 break;
             
@@ -46,8 +48,19 @@ class OrderRequest extends FormRequest
     public function messages()
     {
         return [
+            'out_trade_no.required' => '外部订单号不能为空',
+            'out_trade_no.max' => '外部订单号最大支持64个字符',
+            'out_trade_no.unique' => '外部订单号重复',
             'subject.required' => '支付标题不能为空'
-            'subject.max' => '支付标题最多支持255个字符',
+            'subject.max' => '支付标题最大支持255个字符',
+            'amount.required' => '订单金额最大支持255个字符',
+            'amount.integer' => '订单金额仅支持数字',
+            'amount.min' => '订单金额至少是0.01元',
+            'payway.required' => '付款方式不能为空',
+            'payway.string' => '付款方式格式不正确',
+            'payway.in' => '付款方式仅支持wechat_mini',
+            'notify_url.required' => '回调URL不能为空',
+            'notify_url.url' => '回调URL格式错误',
         ];
     }
 }
