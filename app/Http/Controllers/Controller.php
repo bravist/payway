90caea292014df1603507cc7d948796f55453409
程会勇 <chenghuiyong@ruoyubuy.com>
2018-09-29 14:23:12 +0800

    ApiSignGuard

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,11 +7,6 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-use League\OAuth2\Server\ResourceServer;
-use Illuminate\Http\Request;
-use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
-use Laravel\Passport\TokenRepository;
-
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
