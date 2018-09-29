ce70de2f31599571efb1e203b335a674f9feb381
张露 <zhanglu@ruoyubuy.com>
2018-09-29 21:43:59 +0800

    更新api-auth库

--- config/apiauth.php
+++ config/apiauth.php
@@ -12,41 +12,90 @@
 return [
     /*
      |--------------------------------------------------------------------------
-     | api token auth status
+     | api token auth service config
      |--------------------------------------------------------------------------
-     |
-     | This value determines the api token auth is enabled, or disabled.
-     |
      */
-    'enabled' => env('SIGNATURE_ENABLED', true),
+    'service' => [
+        /*
+         |--------------------------------------------------------------------------
+         | api token auth status
+         |--------------------------------------------------------------------------
+         |
+         | This value determines the api token auth is enabled, or disabled.
+         |
+         */
+        'enabled' => env('API_AUTH_ENABLED', true),
+        
+        /*
+         |--------------------------------------------------------------------------
+         | hashing algorithm
+         |--------------------------------------------------------------------------
+         |
+         | Specify the hashing algorithm that will be used to sign the token.
+         |
+         */
+        'algo' => env('API_AUTH_SERVICE_HMAC_ALGO', 'sha256'),
+        
+        /*
+         |--------------------------------------------------------------------------
+         | Secret Column
+         |--------------------------------------------------------------------------
+         |
+         | The name of the secret "column" in persistent storage.
+         |
+         */
+        'storage_key' => env('API_AUTH_STORAGE_KEY', 'secret'),
+        
+        /*
+        |--------------------------------------------------------------------------
+        | Token Query String
+        |--------------------------------------------------------------------------
+        |
+        | The name of the query string item from the request containing the API token.
+        |
+        */
+        'input_key' => env('API_AUTH_INPUT_KEY', 'api_token'),
+    ],
     
     /*
      |--------------------------------------------------------------------------
-     | hashing algorithm
+     | api token auth client config
      |--------------------------------------------------------------------------
-     |
-     | Specify the hashing algorithm that will be used to sign the token.
-     |
      */
-    'algo' => env('SIGNATURE_HMAC_ALGO', 'sha256'),
-    
-    /*
-     |--------------------------------------------------------------------------
-     | Secret Column
-     |--------------------------------------------------------------------------
-     |
-     | The name of the secret "column" in persistent storage.
-     |
-     */
-    'storage_key' => env('SIGNATURE_STORAGE_KEY', 'secret'),
-    
-    /*
-    |--------------------------------------------------------------------------
-    | Token Query String
-    |--------------------------------------------------------------------------
-    |
-    | The name of the query string item from the request containing the API token.
-    |
-    */
-    'input_key' => env('SIGNATURE_INPUT_KEY', 'api_token'),
+    'client' => [
+        
+        /*
+         |--------------------------------------------------------------------------
+         | hashing algorithm
+         |--------------------------------------------------------------------------
+         |
+         | Specify the hashing algorithm that will be used to sign the token.
+         |
+         */
+        'algo' => env('API_AUTH_CLIENT_HMAC_ALGO', 'sha256'),
+        
+        /*
+         |--------------------------------------------------------------------------
+         | Authentication App ID
+         |--------------------------------------------------------------------------
+         |
+         | Don't forget to set this in your .env file.
+         |
+         */
+        'appid' => env('API_AUTH_APPID'),
+        
+        /*
+         |--------------------------------------------------------------------------
+         | Authentication Secret
+         |--------------------------------------------------------------------------
+         |
+         | Don't forget to set this in your .env file, as it will be used to sign
+         | your tokens. A helper command is provided for this:
+         | `php artisan apiauth:secret`
+         |
+         | Note: This will be used for Symmetric algorithms only (HMAC).
+         |
+         */
+        'secret' => env('API_AUTH_SECRET'),
+    ],
 ];
